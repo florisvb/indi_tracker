c953a062e169a4fd8008f7c049857460d4b3b706
florisvb <florisvb@gmail.com>
2017-08-22 15:27:07 -0700

    first commit

---
+++ indi_tracker_analysis/__init__.py
@@ -0,0 +1,2 @@
+import calibrate_gphoto2_camera
+import find_flies_in_image_directory
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+name='IndiTrackerAnalysis',
+version='0.0.1',
+author='Floris van Breugel',
+author_email='floris@gmail.com',
+packages = ['indi_tracker_analysis'],
+license='BSD',
+description='Analysis scripts for data collected with indi tracker',
+long_description=open('README.md').read(),
+)
